ebe97691fd069c74672d707007cad4ddfd49b2b5
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-18 13:05:09 -0700

    Watch followers

--- twotter/src/App.vue
+++ twotter/src/App.vue
@@ -23,6 +23,13 @@ export default {
       }
     }
   },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
   computed: {
     fullName() {
       return `${this.user.firstName} ${this.user.lastName}`;
