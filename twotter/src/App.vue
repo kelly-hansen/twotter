6fac3a33bf917ea6c4366dd81145a69f3946fb87
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-18 11:52:22 -0700

    Remove Vue default display data

--- twotter/src/App.vue
+++ twotter/src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Twotter"/>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
